ea51b33a682a5c2f7ddb3f0f6f1e0d44a0240ae0
gaonispildoras <srgaona1@gmail.com>
2020-12-28 16:29:28 +0100

    Ya recogemos todas las variables de los 4 formularios y tenemos en SESSION el IDVIAJE

--- formulario/nuevoviaje.php
+++ formulario/nuevoviaje.php
@@ -1,28 +1,64 @@
 <?php 
+error_reporting(0);
+
 //INCLUDES
 include("../php/conexion.php");
 include("../php/sesion.php");
+include("../php/funciones.php");
 
 if (isset($_POST["enviar"])){
-  
+ 
+//PASO 1
 $nviaje=$_POST["nviaje"];
 $fecha_ini=$_POST["fecha_ini"];
 $fecha_fin=$_POST["fecha_fin"];
 $bote=$_POST["bote"];
 
-$consulta_viaje="INSERT INTO viajes (id_usuario, nombre_viaje, fecha_inicio, fecha_fin, bote) VALUES ('$_SESSION[id_usuario]', '$nviaje', '$fecha_ini', '$fecha_fin', '$bote')";
-$consulta_insertar=mysqli_query($conexion, $consulta_viaje);
+//PASO 2
+$transporte=$_POST["transporte"];
+$cantidad_coches=$_POST["cantidad_coche"];
+$combustible=$_POST["combustible"];
+
+//PASO 3
+$estancia=$_POST["estancia"];
+$direccion_estancia=$_POST["d_estancia"];
+$telefono_estancia=$_POST["tlfn_estancia"];
+
+//PASO 4
+$sugerencias_comida=preg_split("/(\,)/", $_POST["comida"]);
+$sugerencias_bebida=preg_split("/(\,)/", $_POST["bebida"]);
+$sugerencias_utensilios=preg_split("/(\,)/", $_POST["utensilios"]);
+$sugerencias_general=preg_split("/(\,)/", $_POST["general"]);
+
+//Generar URL
+echo "Esta es la dirección del viaje: "; echo "<br>";
+echo "<a href='../viaje_creado/viaje_creado.php'>Localhost:8080/Proyecto_iViaje/viaje_creado/viaje_creado.php/$_SESSION[id_viaje]</a>";
 
 
 
 
+
+//INSERT PASO 1
+$insertar_viaje="INSERT INTO viajes (id_usuario, nombre_viaje, fecha_inicio, fecha_fin, bote) VALUES ('$_SESSION[id_usuario]', '$nviaje', '$fecha_ini', '$fecha_fin', '$bote')";
+mysqli_query($conexion, $insertar_viaje);
+
+$sql="SELECT idviaje FROM viajes WHERE id_usuario=$_SESSION[id_usuario]";
+$consulta_id_viaje=mysqli_query($conexion, $sql);
+    while ($id_viaje=mysqli_fetch_row($consulta_id_viaje)){
+        $array_id_viaje[]=$id_viaje[0];
+        
+    }
+$_SESSION["id_viaje"]=end($array_id_viaje); //Con esto tenemos en SESSION el ID VIAJE generado por la consulta anterior.
+    
+    
+
+//INSERT PASO 2
+
+
+//INSERT PASO 3
+
+//INSERT PASO 4
 
 
 }
-
-
-
-
-
-
 ?> 
