684da3bdbbbd9cf7597eb801774ba500bf091fee
gaonispildoras <srgaona1@gmail.com>
2020-12-28 16:27:59 +0100

    Se añade el archivo funciones (La primera funcion que tiene es hacer una consulta simple con un select

---
+++ php/funciones.php
@@ -0,0 +1,17 @@
+<?php
+//INCLUDES
+
+
+//FUNCION SELECT SQL
+function select_query($campo,$tabla,$where1,$where2){
+        include("conexion.php");
+
+        $sql="SELECT $campo FROM $tabla WHERE $where1='$where2'";
+        $resultado1=mysqli_query($conexion, $sql);
+        $resultado2=mysqli_fetch_row($resultado1);
+
+        return $resultado2;
+}
+
+
+?>
