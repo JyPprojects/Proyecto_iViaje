379d046bf6e03e9a340883ad1c7594b0cd14aeb5
gaonispildoras <srgaona1@gmail.com>
2021-01-31 20:39:49 +0100

    Se añaden las consultas para crear las TABLAS DINÁMICAS

--- viaje_creado/viaje_creado.php
+++ viaje_creado/viaje_creado.php
@@ -11,4 +11,11 @@ $info_comida=select_query_necesidad("necesidad_viaje", "necesidades_viaje", "id_
 $info_bebida=select_query_necesidad("necesidad_viaje", "necesidades_viaje", "id_viaje", "$_SESSION[id_viaje]", "id_necesidad", "2");
 $info_utensilios=select_query_necesidad("necesidad_viaje", "necesidades_viaje", "id_viaje", "$_SESSION[id_viaje]", "id_necesidad", "3");
 $info_general=select_query_necesidad("necesidad_viaje", "necesidades_viaje", "id_viaje", "$_SESSION[id_viaje]", "id_necesidad", "4");
+
+$sql="SELECT id_necesidad_viaje, id_usuario, necesidad_viaje FROM necesidades_viaje WHERE id_necesidad = 1 AND id_viaje = $_SESSION[id_viaje]";
+$query=mysqli_query($conexion, $sql);
+
+$sql2="SELECT id_usuario, necesidad_viaje FROM necesidades_viaje WHERE id_necesidad = 2 AND id_viaje = $_SESSION[id_viaje]";
+$query2=mysqli_query($conexion, $sql2);
+
 ?>
