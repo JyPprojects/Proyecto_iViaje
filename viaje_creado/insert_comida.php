869902f4cf63d69fd9197ee679090b0c161dcb78
gaonispildoras <srgaona1@gmail.com>
2021-05-22 00:24:14 +0200

    Se soluciona el fallo que agregaba campos vacíos

--- viaje_creado/insert_comida.php
+++ viaje_creado/insert_comida.php
@@ -1,13 +1,19 @@
 <?php
 include("../php/conexion.php");
+include("../php/funciones.php");
 
-$id_viaje = preg_replace("/\_/", " ", $_GET["id_viaje"]);
-$usuario_nuevo = preg_replace("/\_/", " ", $_GET["usuario_nuevo"]);
-$input_nuevo = preg_replace("/\_/", " ", $_GET["input_nuevo"]);
+$resultado = select_query("necesidad_viaje", "necesidades_viajes", "necesidad_viaje", "$_GET[input_nuevo]");
+echo $resultado[0];
 
-$sql="INSERT INTO necesidades_viaje (id_viaje, id_necesidad, id_usuario, necesidad_viaje) VALUES ('$id_viaje' , 1 , '$usuario_nuevo' , '$input_nuevo')";
-$consulta=mysqli_query($conexion, $sql);
 
+if (!empty($_GET["input_nuevo"])){
+    $id_viaje = preg_replace("/\_/", " ", $_GET["id_viaje"]);
+    $usuario_nuevo = preg_replace("/\_/", " ", $_GET["usuario_nuevo"]);
+    $input_nuevo = preg_replace("/\_/", " ", $_GET["input_nuevo"]);
+
+    $sql="INSERT INTO necesidades_viaje (id_viaje, id_necesidad, id_usuario, necesidad_viaje) VALUES ('$id_viaje' , 1 , '$usuario_nuevo' , '$input_nuevo')";
+    $consulta=mysqli_query($conexion, $sql);
+}
 
 
 ?>
