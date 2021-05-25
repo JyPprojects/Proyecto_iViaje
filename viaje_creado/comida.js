5c2a2a8065a354b234702fd7f51fb79969a446b1
gaonispildoras <srgaona1@gmail.com>
2021-05-26 00:05:15 +0200

    Se soluciona el BUG que nos obligaba a clicar 2 o mas veces para que se guardasen los cambios en los inputs.
    SOLO FALTA por arreglar el fallo de que al clicar varias veces en el input se añaden muchas filas a la BBDD.

--- viaje_creado/comida.js
+++ viaje_creado/comida.js
@@ -1,4 +1,3 @@
-        //COMIDA
     $(function(){
         $(".input_viaje").hide(); 
         $(".p_oculto").hide();  
@@ -6,22 +5,22 @@
         $("#sesion_idusuario").hide();  
         $("#sesion_idviaje").hide();   
 
-        // COMO AQUI TENGO PUESTO DOCUMENT CUALQUIER CLIC QUE HAGA EN TODO EL DOCUMENTO DESENCADENARA EL EVENTO. (HAY QUE CAMBIARLO)
+        // UPDATE DE LOS DATOS DE LA TABLA
        $("#tabla").click(function(event)  { 
             var target = $(event.target); 
+            console.log(target);
             var id = target.attr("id"); 
 
                 if (id){
                     var id2 = id.split("_");
-                    var id3 = id2[1];
-                    var input = $("#i_"+id3).val(); 
+                    var id3 = id2[1];               
                     var id_oculto = $("#id_oculto_"+id3).text();
-
                              
                     $("#p_"+id3).hide(); 
                     $("#i_"+id3).show();
 
                     $("#i_"+id3).on("blur", function(){
+                        var input = $("#i_"+id3).val(); 
                         $("#i_"+id3).hide(); 
                                         
                         $("#p_"+id3).text(input);
@@ -36,27 +35,28 @@
                     });                                                        
                 }       
         });
+        // AÑADIR UN NUEVO CAMPO
         var i = 0;
         $("#añadir").click(function(){
             i++;
             
             var usuario=$("#sesion_usuario").text();
-            var prueba1 = '<tr>'+ '<td><p id="p_nuevo'+i+'"></p> <input id="input_nuevo'+i+'" type="text"></td>' + '<td>' +usuario+ '</td>' + '<tr>';
-            $("#p_nuevo").hide();
-            $("#tabla").append(prueba1);
+            var fila = '<tr>'+ '<td><p id="p_nuevo'+i+'"></p> <input id="input_nuevo'+i+'" type="text"></td>' + '<td>' +usuario+ '</td>' + '<td> <button type="button" class="btn btn-danger">Eliminar</button> </td>' + '<tr>';
+            $("#tabla").append(fila);
 
         });
-        $(document).click(function(){
+        // INSERTAR NUEVO CAMPO
+        $("#tabla").click(function(){
             var id_usuario=$("#sesion_idusuario").text();  
             var id_viaje=$("#sesion_idviaje").text();   
-            var i_nuevo = $("#input_nuevo"+i).val();
         
             $("#input_nuevo"+i).on("blur", function(){
-                $("#p_nuevo"+i).text(i_nuevo);
+                var i_nuevo2 = $("#input_nuevo"+i).val();
+                $("#p_nuevo"+i).text(i_nuevo2);
                 $("#input_nuevo"+i).hide();
                 $("#p_nuevo"+i).show();
                 
-                var enlace="insert_comida.php?input_nuevo="+i_nuevo+"&id_viaje="+id_viaje+"&usuario_nuevo="+id_usuario+"";
+                var enlace="insert_comida.php?input_nuevo="+i_nuevo2+"&id_viaje="+id_viaje+"&usuario_nuevo="+id_usuario+"";
                 var enlace2= enlace.replace(/ /g, "_");
                 $("#resultado").load(enlace2);
             });         
