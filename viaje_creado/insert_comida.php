4df4bfd368d563f50e3e4a36eae9976a938791e6
gaonispildoras <srgaona1@gmail.com>
2021-05-27 21:48:14 +0200

    Se consigue que no se metan valores duplicados, lo cual elimina el bug de insertar valores cada vez que hacemos click en el input nuevo.

--- viaje_creado/insert_comida.php
+++ viaje_creado/insert_comida.php
@@ -2,11 +2,10 @@
 include("../php/conexion.php");
 include("../php/funciones.php");
 
-// $resultado = select_query("necesidad_viaje", "necesidades_viajes", "necesidad_viaje", "$_GET[input_nuevo]");
-// echo $resultado[0];
+$resultado = select_query("necesidad_viaje", "necesidades_viaje", "necesidad_viaje", "$_GET[input_nuevo]");
 
 
-if (!empty($_GET["input_nuevo"])){
+if (!empty($_GET["input_nuevo"]) && empty($resultado[0])){
     $id_viaje = preg_replace("/\_/", " ", $_GET["id_viaje"]);
     $usuario_nuevo = preg_replace("/\_/", " ", $_GET["usuario_nuevo"]);
     $input_nuevo = preg_replace("/\_/", " ", $_GET["input_nuevo"]);
@@ -14,6 +13,10 @@ if (!empty($_GET["input_nuevo"])){
     $sql="INSERT INTO necesidades_viaje (id_viaje, id_necesidad, id_usuario, necesidad_viaje) VALUES ('$id_viaje' , 1 , '$usuario_nuevo' , '$input_nuevo')";
     $consulta=mysqli_query($conexion, $sql);
 }
+else{
+    echo "por ahí no paso";
+}
+
 
 
 ?>
