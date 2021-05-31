7085c00a2a413a9919b01a9ecd9e12212d49b801
TitiJoao <juanan459@gmail.com>
2021-05-31 12:29:28 +0200

    Se ponen todos los favicon, menos en la ultima pagina

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@ include("php/sesion.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="menu_post-login/Imagenes/favicon1.ico">
+    <link rel="icon" href="menu_post-login/Imagenes/favicon5.ico">
     <title>MAKE AND TRAVEL | <?php echo ucfirst($_SESSION["usuario"]); ?> </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
