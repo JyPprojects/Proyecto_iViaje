c933dca5a2898619c4a7ba6b84b8e37af8ac4516
TitiJoao <juanan459@gmail.com>
2021-06-02 16:29:57 +0200

    Se añade el boton de admin en el desplegable del usuario solo para los usuarios titijuanan y gaonis

--- Index.php
+++ Index.php
@@ -42,12 +42,7 @@ include("php/sesion.php");
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <?php echo ucfirst($_SESSION["usuario"]); ?>
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="ajustes_perfil/index.php">Ajustes</a></li>
-                <li><a class="dropdown-item" href="#">Acerca de</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="php/sesion.php?exit='exit'">Cerrar Sesión</a></li>
-              </ul>
+              <?php include("php/admin.php"); ?> 
             </li>
       </ul> 
     </div>
---
+++ php/admin.php
@@ -0,0 +1,23 @@
+<?php 
+if ($_SESSION["usuario"]=="Titijuanan" || $_SESSION["usuario"]=="gaonis") {
+?>
+<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <li><a class="dropdown-item" href="ajustes_perfil/index.php">Ajustes</a></li>
+    <li><a class="dropdown-item" href="Admin/index.php">Admin</a></li>
+    <li><a class="dropdown-item" href="#">Acerca de</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="php/sesion.php?exit='exit'">Cerrar Sesión</a></li>
+</ul>
+<?php } 
+
+else {?>
+<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <li><a class="dropdown-item" href="ajustes_perfil/index.php">Ajustes</a></li>
+    <li><a class="dropdown-item" href="#">Acerca de</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="php/sesion.php?exit='exit'">Cerrar Sesión</a></li>
+</ul>
+<?php } ?>
+
+
+
