0eade7067edab03039b800a388cd25b976b13698
TitiJoao <juanan459@gmail.com>
2021-06-03 22:46:09 +0200

    Se actualizan todas las barras de navehgacion menos en la ultima pagina

--- Admin/index.php
+++ Admin/index.php
@@ -18,20 +18,20 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../index.php">MAKE AND TRAVEL</a>
+    <a class="navbar-brand" href="../index.php"><img src="../menu_post-login/Imagenes/logo2.png" alt="" width="30" height="24" class="d-inline-block align-text-top">MAKE AND TRAVEL</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../index.php">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Acerca de</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Contactanos</a>
         </li>
       </ul>
       <ul class="navbar-nav me-righ mb-2 mb-lg-0" style="margin-right: 5%;">
@@ -51,8 +51,9 @@
   </div>
 </nav>
 
+<div>
 <h1 class="titulo">Usuarios Registrados</h1>
-<table class="table table-striped table-sm">
+<table class="table table-striped">
   <thead>
     <tr>
       <th scope="col">Id</th>
@@ -78,6 +79,7 @@
     ?>
   </tbody>
 </table>
+</div>
 
 <br>
 
