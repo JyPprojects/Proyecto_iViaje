dd239c907e57156eb92dd7ce18a0d4528f41e168
TitiJoao <juanan459@gmail.com>
2021-06-07 12:10:05 +0200

    Ahora si que se arregla la barra de navegacion xd

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -54,10 +54,10 @@
                 <?php echo ucfirst($_SESSION["usuario"]); ?>
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="ajustes_perfil/index.php">Ajustes</a></li>
+                    <li><a class="dropdown-item" href="../ajustes_perfil/index.php">Ajustes</a></li>
                     <li><a class="dropdown-item" href="#">Acerca de</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="php/sesion.php?exit='exit'">Cerrar Sesión</a></li>
+                    <li><a class="dropdown-item" href="../php/sesion.php?exit='exit'">Cerrar Sesión</a></li>
                 </ul>
             </li>
       </ul> 
